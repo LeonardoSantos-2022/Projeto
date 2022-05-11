dac06e62665ac2a6dcdd242aa1f5cdaa7785c6ec
LeonardoSantos-2022 <leonunessan@gmail.com>
2022-05-11 18:26:33 -0300

    projeto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 botao=document.querySelector('.botao-menu');
 menuChaveado=document.querySelector('.menu-navebar');
+menuH=document.querySelector('.botao-menu')
 
 
 botao.addEventListener('click',function(){
     menuChaveado.classList.toggle('menu-active')
+    menuH.classList.toggle('active')
 })
