c9c1a402dcfb47b19eddde9454a19311d977b29d
LeonadoSantos2022 <Leonunessan@gmail.com>
2023-11-22 18:59:15 -0300

    projeto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 botao=document.querySelector('.botao-menu');
 menuChaveado=document.querySelector('.botao-menu');
-menuH=document.querySelector('.menu-mobile')
+const menuH=document.querySelector('.menu-mobile')
 
 botao.addEventListener('click',function(){
     menuChaveado.classList.toggle('active')
-    menuH.classList.toggle('menu-active')
+    menuH.classList.toggle('menu-active') 
+    if (!menuH.classList.contains("menu-active")) {
+        document.querySelector('body').style.overflow="hidden"
+    }else{ 
+        document.querySelector('body').style.overflow="visible"
+    }
+})
+navbar=document.querySelector('.cabecario')
+window.addEventListener('scroll',function(){
+if (this.scrollY > 0) {
+    navbar.style.top='-130px';
+    navbar.style.transition='all .5s ease-in';
+}else{
+   navbar.style.top='0' ;
+   navbar.style.transition='all .3s ease-in';
+}
 })
