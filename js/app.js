d5c692edfdb2bcb01117a9d0caa2822b3f31e663
LeonardoSantos-2022 <leonunessan@gmail.com>
2026-05-15 23:44:03 -0300

    ajuste menu mobile e  logo diminuida

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 botao=document.querySelector('.botao-menu');
 menuChaveado=document.querySelector('.botao-menu');
-const menuH=document.querySelector('.menu-mobile')
+navbar=document.querySelector('.cabecario')
+const menuH=document.querySelector('.menu-mobile');
+
 
 botao.addEventListener('click',function(){
     menuChaveado.classList.toggle('active')
     menuH.classList.toggle('menu-active') 
     if (!menuH.classList.contains("menu-active")) {
-        document.querySelector('body').style.overflow="hidden"
+        document.querySelector('body').style.overflow="hidden";
+        navbar.style.borderBottom='none';
     }else{ 
-        document.querySelector('body').style.overflow="visible"
+        document.querySelector('body').style.overflow="visible";
+          navbar.style.borderBottom='5px solid rgb(54, 54, 54)';
     }
 })
-navbar=document.querySelector('.cabecario')
 window.addEventListener('scroll',function(){
-if (this.scrollY > 0 && this.innerWidth > 992) {
+if (this.scrollY > 2 && this.innerWidth > 992) {
     navbar.style.top='-130px';
-    navbar.style.transition='all .5s ease-in';
+   navbar.style.transition='top .1s ease-out';
 }else{
    navbar.style.top='0' ;
-   navbar.style.transition='all .3s ease-in';
+   navbar.style.transition='top .1s ease-out';
 }
 })
