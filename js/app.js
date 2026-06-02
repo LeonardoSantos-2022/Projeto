64b74c05c8bfdfcc3f4151995cc6c07f0ad91cb7
LeonardoSantos-2022 <leonunessan@gmail.com>
2026-06-02 00:24:58 -0300

    alterações todas paginas com estilo e funções part 1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 botao=document.querySelector('.botao-menu');
 menuChaveado=document.querySelector('.botao-menu');
-navbar=document.querySelector('.cabecario')
-const menuH=document.querySelector('.menu-mobile');
+navbar=document.querySelector('.cabecario');
+footer=document.querySelector('.rodapé');
 
+const menuH=document.querySelector('.menu-mobile');
+const btnRetornUp=document.getElementById('btnRetornUp');
+const btnRetornUpStyle=document.querySelector('.btnRetorn > i');
 
 botao.addEventListener('click',function(){
     menuChaveado.classList.toggle('active')
@@ -15,12 +18,43 @@ botao.addEventListener('click',function(){
           navbar.style.borderBottom='5px solid rgb(54, 54, 54)';
     }
 })
-window.addEventListener('scroll',function(){
-if (this.scrollY > 2 && this.innerWidth > 992) {
+
+const posFooter=footer.getBoundingClientRect().height
+
+window.addEventListener('scroll',function(e){
+    
+    const movePagina=this.window.scrollY
+    const docHeight=window.document.body.offsetHeight
+    const posBtnFixed=btnRetornUp.getBoundingClientRect().height
+    const posBtnFixedTop=btnRetornUpStyle.getBoundingClientRect().top
+    const calcHeight=parseInt((docHeight-posFooter)-posBtnFixed-posBtnFixedTop)
+    
+    if (movePagina>=calcHeight) {
+        btnRetornUpStyle.style.backgroundColor='white';
+        btnRetornUpStyle.style.color='black';
+    }else{
+        btnRetornUpStyle.style.backgroundColor='black';
+        btnRetornUpStyle.style.color='white';
+    }
+
+if (this.scrollY > 2) {
     navbar.style.top='-130px';
    navbar.style.transition='top .1s ease-out';
+    btnRetornUp.style.display='flex'
 }else{
    navbar.style.top='0' ;
    navbar.style.transition='top .1s ease-out';
+   btnRetornUp.style.display='none'
 }
 })
+
+btnRetornUp.addEventListener('click',function(){
+    window.scroll(top,0)
+})
+
+
+
+
+
+
+
